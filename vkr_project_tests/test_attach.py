8638cef02314ce21827ad922db7e286fdc0da4ea
Ярослав Сухоруков <y.sukhorukov@iva-tech.ru>
2022-04-08 22:14:54 +0300

    new

--- vkr_project_tests/test_attach.py
+++ vkr_project_tests/test_attach.py
@@ -12,3 +12,10 @@ def test_xfail_unexpected_pass():
     """this test is an xfail that will be marked as unexpected success"""
     assert True
 
+@pytest.mark.xfail(condition=lambda: True, reason='this test is expecting failure')
+def test_xfail_unexpected_pass__():
+    """this test is an xfail that will be marked as unexpected success"""
+    assert True
+
+
+
