f7567dce35bf189e38faf16f1095c82ab43c48b4
Ярослав Сухоруков <y.sukhorukov@iva-tech.ru>
2022-04-10 20:12:11 +0300

    add tests case

--- vkr_project_tests/test_api_direct.py
+++ vkr_project_tests/test_api_direct.py
@@ -8,5 +8,5 @@ import pytest
 @allure.step('API Direct test')
 @pytest.mark.parametrize('step', ['general request', 'query by day', ''])
 def test_api_direct(step):
-    time.sleep(random.randint(20, 60))
+    time.sleep(random.randint(1, 5))
     assert step in ['general request', 'query by day', '']
--- vkr_project_tests/test_internal_API.py
+++ vkr_project_tests/test_internal_API.py
@@ -6,5 +6,5 @@ import pytest
 
 @pytest.mark.parametrize('identifier', ['1', '2', '3', '4', '5', '6', '7', '8'])
 def test_api_direct(identifier):
-    time.sleep(random.randint(20, 60))
+    time.sleep(random.randint(1, 5))
     assert identifier in ['1', '2', '3', '4', '5', '6', '7', '8']
