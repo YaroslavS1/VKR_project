ac6f171cc2e2233ce99641966d7e988bcfae2905
Ярослав Сухоруков <y.sukhorukov@iva-tech.ru>
2022-04-10 14:08:44 +0300

    add tests case

--- vkr_project_tests/test_attach.py
+++ vkr_project_tests/test_attach.py
@@ -10,17 +10,11 @@ import pytest
 LOGGER = logging.getLogger(__name__)
 
 
-@pytest.mark.xfail(condition=lambda: True, reason='this test is expecting failure')
-def test_count_summary_cost_ads():
+@pytest.mark.parametrize('file', ['cashe0.csv', 'cashe1.csv', 'cashe2.csv'])
+def test_count_summary_cost_ads(file):
     """Calculation of the total cost of advertising"""
-    '''
-    суть теста разложить сколько мы заплатили по дням 
-    распечатать это 
-    и ссумировать 
-    '''
     LOGGER.debug(f'{os. getcwd()}')
-    print(os. getcwd())
-    f_data = pd.read_csv("./case/cashe.csv", header=1, sep='	', index_col=0, )
+    f_data = pd.read_csv(f'./case/{file}', header=1, sep='	', index_col=0)
     f_data['Cost'] = pd.to_numeric(f_data["Cost"], downcast='float')
     f_data['Cost'] = f_data['Cost'] / 1000000
     summary_cost = float(f_data.groupby('CampaignName').sum().reset_index()['Cost'])
@@ -28,19 +22,13 @@ def test_count_summary_cost_ads():
     trace1 = px.line(
         x=f['Date'],
         y=f['Cost'],)
-    # fig.add_trace(trace1, row=1, col=1)
-    # # fig.add_trace(trace2, row=2, col=1)
-    # # fig.add_trace(trace3, row=3, col=1)
-    #
-    # fig.update_layout(xaxis=dict(tickangle=90))
     temp = tempfile.NamedTemporaryFile()
-    print(temp.name)
-    # fp.write()
     trace1.write_html(f'{temp.name}')
     allure.attach.file(f'{temp.name}', attachment_type=allure.attachment_type.HTML)
+
     print((sum(f['Cost'])))
     print(summary_cost)
-    # assert False
+    assert round(summary_cost, 5) == round(sum(f['Cost']), 5)
 
 
 @pytest.mark.xfail(condition=lambda: True, reason='this test is expecting failure')
