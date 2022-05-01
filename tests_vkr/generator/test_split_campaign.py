3fe955699c061583f124a9ce8617241c5140d80f
y_sukhorukov <y.sukhorukov@iva-tech.ru>
2022-05-01 08:18:52 +0300

    add crm and other minor fix

--- tests_vkr/generator/test_split_campaign.py
+++ tests_vkr/generator/test_split_campaign.py
@@ -19,21 +19,21 @@ from ..tools.CRM import CrmReport
         # '25.05.2010',
         '01.01.2000',
     ])
-@pytest.mark.parametrize('profit1', [8000, 1, 10000000000, 123, 50.50, 100.01, 0.1])
-@pytest.mark.parametrize('profit2', [8000, 1, 10000000000, 123, 50.50, 100.01, 0.1])
-@pytest.mark.parametrize('clicks1', [8000, 1, 10000000000, 123])
-@pytest.mark.parametrize('clicks2', [8000, 1, 10000000000, 123])
+@pytest.mark.parametrize('profit1', [9873, 1, 1000000, 50.50, 0.1])
+@pytest.mark.parametrize('profit2', [8764, 0, 1000000, 50.50, 0.1])
+@pytest.mark.parametrize('clicks1', [8000, 1])
+@pytest.mark.parametrize('clicks2', [8000, 1])
 def test_split_campaign_crm(start_date, end_date, profit1, profit2, clicks1, clicks2):
     'сделать более гибкое количество рекламных компаний'
     camaign_1 = AdvContextCRM('yandex', 'test_campaign1', profit1, clicks1)
     camaign_2 = AdvContextCRM('yandex', 'test_campaign2', profit2, clicks2)
-    camaign_3 = AdvContextCRM('yandex', 'test_campaign3', profit1, clicks1)
-    camaign_4 = AdvContextCRM('yandex', 'test_campaign4', profit2, clicks2)
+    # camaign_3 = AdvContextCRM('yandex', 'test_campaign3', profit1, clicks1)
+    # camaign_4 = AdvContextCRM('yandex', 'test_campaign4', profit2, clicks2)
 
     report_ = CrmReport(
         start_date=start_date,
         end_date=end_date,
-        config=(camaign_1, camaign_2, camaign_3, camaign_4))
+        config=(camaign_1, camaign_2))
 
     result = report_._split_by_company
     summary_profit = 0
@@ -41,5 +41,5 @@ def test_split_campaign_crm(start_date, end_date, profit1, profit2, clicks1, cli
     for i in result:
         summary_profit += sum(result[i][0])
         summary_clicks += sum(result[i][1])
-    assert abs(summary_profit - (profit1 + profit2) * 2) < 1e-4
-    assert summary_clicks == (clicks1 + clicks2) * 2
+    assert abs(summary_profit - (profit1 + profit2)) < 1e-4
+    assert summary_clicks == (clicks1 + clicks2)
